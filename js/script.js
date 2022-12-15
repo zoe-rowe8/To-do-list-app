94e7f05f8c3af873d22b6a7e9ceadef5510f53ae
zoe-rowe8 <zoemarierowe@hotmail.co.uk>
2022-12-15 12:21:01 +0000

    Crossing out an item

--- js/script.js
+++ js/script.js
@@ -14,11 +14,13 @@ function newItem(){
     }
     
      //2. Crossing out an item from the list of items:
-       function crossOut() {
-             li.classList.toggle("strike");
-         }
+    function crossOut() {
+       li.toggleClas("strike");
+    }
     
-         li.addEventListener("dblclick",crossOut);
+    li.on("dblclick", function crossOut() {
+      li.toggleClass("strike");
+    });
     
      //3(i). Adding the delete button "X": 
        let crossOutButton = document.createElement("crossOutButton");
